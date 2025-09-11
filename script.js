bba0877923689d636558a800e73547376e486a17
ismoo14 <ismailabdiaman4@gmail.com>
2025-09-11 17:20:00 +0000

    added some code

--- script.js
+++ script.js
@@ -24,3 +24,67 @@ rockbtn.addEventListener("click", function(){
     sect2.style.display = "none"
     hero2.style.display = "block"
 })
+
+rulebtn.addEventListener("click", function(){
+    sect3.style.display = "block"
+    hero2.style.display = "none"
+    hero.classList.add("hero-121")
+    sect2.style.display = "block"
+})
+
+closebtn.addEventListener("click", function(){
+    sect3.style.display = "none"
+    sect2.style.display = "block"
+    hero.classList.remove("hero-121")
+    sect2.classList.remove("section-221")
+})
+
+playbtn.addEventListener("click", function(){
+    sect2.style.display = "block"
+    hero2.style.display = "none"
+})
+
+
+
+const img1 = document.querySelector(".img-1")
+const img2 = document.querySelector(".img-2")
+const rev = [
+    {
+        id: 1,
+        name: "paper",
+        name2: "papery",
+        img:"images/icon-paper.svg",
+    },
+    {
+        id: 2,
+        name: "scissor",
+        name2: "scs",
+        img:"images/icon-scissors.svg",
+    },
+    {
+        id: 3,
+        name: "rock",
+        name2: "rockkkkk",
+        img:"images/icon-rock.svg",
+    },
+]
+
+const currentitem = 0;
+
+window.addEventListener("DOMContentLoaded", function(){
+    show(currentitem)
+});
+
+function show(planets){
+    const item = rev[planets];
+    img1.src = item.img;
+
+}
+
+const txt = document.querySelector(".text")
+
+const value = document.querySelector(".value")
+
+let count = 0;
+
+const lftbtn = document.querySelector(".left-btn")
